5c7d29d9bffe9311b676836bc8cf87c5bc91df46
Vasil Bit <vasil.bituni@gmail.com>
2021-04-23 15:53:50 +0200

    Add edit field

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
+import uniqid from 'uniqid';
 import Overview from './components/Overview';
+import Greeting from './Greeting';
 
 export interface Task {
-  id: number;
+  id: string;
   title: string;
 }
 
-const App = (props: object) => {
+const App = () => {
   const [input, setInput] = React.useState<string>('');
   const [toDos, setTodos] = React.useState<Array<Task>>([]);
+  const [logIn, setLogIn] = React.useState<boolean>(false);
 
   const updateValue = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value);
@@ -17,8 +20,14 @@ const App = (props: object) => {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setTodos([...toDos, { id: Math.random(), title: input }]);
+    setTodos([...toDos, { id: uniqid(), title: input }]);
+    console.log(toDos);
     setInput('');
+  };
+
+  const deleteTask = (id: string) => {
+    const newList = toDos.filter((taks) => taks.id !== id);
+    setTodos(newList);
   };
 
   return (
@@ -26,9 +35,13 @@ const App = (props: object) => {
       <h1>Do-It-Yourself</h1>
       <form onSubmit={handleSubmit}>
         <input type="text" id="input" value={input} onChange={updateValue} />
-        <button>Submit</button>
+        <button type="submit">Submit</button>
       </form>
-      <Overview toDos={toDos} />
+      <button onClick={() => setLogIn(!logIn)}>
+        {logIn ? 'Log Out' : 'Log In'}
+      </button>
+      <Greeting />
+      <Overview toDos={toDos} onClick={(id: string) => deleteTask(id)} />
     </div>
   );
 };
--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -1,12 +1,22 @@
 const Overview = (props: any) => {
   const { toDos } = props;
 
+  function clicked() {
+    console.log('yas');
+  }
+
+  function componenet() {
+    return;
+  }
+
   return (
     <div>
-      {toDos.map((task: any) => (
-        <div>
-          <p key={task.id}>{task.title}</p>
-          <button>Delete</button>
+      {toDos.map((task: any, index: number) => (
+        <div key={task.id}>
+          <span>{index + 1}</span>
+          <p>{task.title}</p>
+          <button onClick={() => props.onClick(task.id)}>Delete</button>
+          <button onClick={clicked}>Edit</button>
         </div>
       ))}
     </div>
